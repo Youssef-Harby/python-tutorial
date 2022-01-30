1dce43c448c961885b6f79e0de5d49e7608e0dd5
Youssef Harby <youssef_harby@yahoo.com>
2022-01-30 08:07:33 +0200

    more then average

--- Day2/PythonDay2TaskITI.py
+++ Day2/PythonDay2TaskITI.py
@@ -1,4 +1,8 @@
 # Taking the number of subjects and storing it in a variable.
+from calendar import c
+from operator import le
+
+
 subnum = int(input("Enter number of subjects :"))
 idstuList = []
 subList = []
@@ -17,50 +21,55 @@ def subgradeSTud():
             #sub range user inputs for student range(5)
             grade = float(
                 input("Student " + str(st) + " Grade Of Sub " + str(sub) + " is :"))
-            print("Grade " + str(grade))
+            # print("Grade " + str(grade))
             subList.append(grade)
-            print("append " + str(subList))
+            # print("append " + str(subList))
         idstuList.append(st)
         idstuList.append(subList)
 
 subgradeSTud()
-print(idstuList)
+# print(idstuList)
 
 
-def dataByID(iid=1):
+def gradeListByID(iid=1):
     ind = idstuList.index(iid)
     return idstuList[ind + 1]  # Return Grade as List [50,60]
 
+# Fun Average Grade By Student ID
+def averGrad(sID):
+    return sum(gradeListByID(sID)) / len(gradeListByID(sID))  # average grade *float 15.0
 
-def averGrad():
-    return sum(dataByID(sID)) / len(dataByID(sID))  # average grade *float 15.0
+# Find the number of students whose grade is more than average
 
+avGradeList = []
+for sID in range(1, 6):
+    avGradeList.append(averGrad(sID))
+    sumGradeAverage = sum(avGradeList)/len(avGradeList)
+    # print (sumGradeAverage)
+    # print(avGradeList)
+
+count = 0
+for i in range(len(avGradeList)):
+    if avGradeList[i] > sumGradeAverage:
+        count = count +1
+print("The Number of Students Whose Grade is More Than Average : " + str(count))
+
+# Grade by ID 
 
 while True:
     try:
         sID = int(input("Enter Student ID :"))
-        if 50 > averGrad() > 0:
+        if 50 > averGrad(sID) > 0:
             print('Fail')
-        elif 50 >= averGrad() < 65:
+        elif 50 >= averGrad(sID) < 65:
             print('Pass')
-        elif 65 >= averGrad() < 75:
+        elif 65 >= averGrad(sID) < 75:
             print('Good')
-        elif 75 >= averGrad() < 85:
+        elif 75 >= averGrad(sID) < 85:
             print('Very good')
-        elif 85 <= averGrad() <= 100:
+        elif 85 <= averGrad(sID) <= 100:
             print('Excellent')
         else:
             print('Invalid Degree %')
     except:
         print("That's not a valid number!")
-
-# Find the number of students whose grade is more than average
-
-# def stuData():
-#     # for studentID in range(0,len(idstuList),2):
-#         # print (idstuList[studentID])
-
-#     for studentGrade in range(1,len(idstuList),2):
-#         print (idstuList[studentGrade])
-        
-# stuData()
